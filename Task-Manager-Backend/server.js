3835b2f182f92eeb890bb7a4c9f8e9380ffcad37
Bhoomi Ahir <bhoomiahir.work@gmail.com>
2026-08-20 23:50:46 +0530

    database connection done

--- Task-Manager-Backend/server.js
+++ Task-Manager-Backend/server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 const auth = require("./routes/auth");
+require("dotenv").config()
+const mongoose = require("mongoose");
+
+// connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error:", err));
 
 app.use(express.json());
 app.use("/api/auth", auth);
